c6fa83dc21a00e2e2a4dac6aa23c16cb532df6a7
100grassfed <mpshurm@gmail.com>
2023-04-06 15:00:20 -0700

    updated fish

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -1,81 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class Fish : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 1.0f;
-    [SerializeField]
-    private float acceleration = 1.0f;
-    [SerializeField]
-    private float minInterval = 1;
-    [SerializeField]
-    private float maxInterval = 8;
-    [SerializeField]
-    private SpriteRenderer spriteRenderer;
-    [SerializeField]
-    private float swimTime = 1.0f;
-    [SerializeField]
-    private string targetTag = "Food";
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float acceleration = 1.0f;
+    [SerializeField] private float minInterval = 1;
+    [SerializeField] private float maxInterval = 8;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private string targetTag = "Food";
+    [SerializeField] private int changeDirectionMin = 1;
+    [SerializeField] private int changeDirectionMax = 7;
+    [SerializeField] private int stomachSize = 50;
+    [SerializeField] private float hungerThreshold = 0.8f;
+    [SerializeField] private int movementCost = 1;
+    [SerializeField] private int foodBonus = 10;
+    [SerializeField] private float moneyFrequency = 30;
+    [SerializeField] private GameObject money;
+
 
     private float changeDirectionInterval;
     private Vector2 movementDirection;
     private float timeSinceLastDirectionChange;
     private Rigidbody2D rb;
-    float closestDistance = Mathf.Infinity;
+    private float closestDistance = Mathf.Infinity;
+    private int stomachHunger;
+    private float moneyTimer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         movementDirection = Random.insideUnitCircle;
         changeDirectionInterval = 0;
+        stomachHunger = 30;
+        moneyTimer = Time.time;
     }
 
     void Update()
     {
-        timeSinceLastDirectionChange += Time.deltaTime;
-        GameObject[] findFood = GameObject.FindGameObjectsWithTag(targetTag);
-        Vector2 closestVector = new Vector2(0,0);
-        float randomInterval = Random.Range(minInterval, maxInterval);
-        Vector2 randomDirection = Random.insideUnitCircle;
-
-        if (findFood.Length != 0)
-        {
-            foreach (GameObject obj in findFood)
-            {
-                Vector2 distance = transform.position - obj.transform.position;
-
-                if (obj == findFood.Last())
-                {
-                    closestDistance = Mathf.Infinity;
-                }
-
-                if (distance.magnitude < closestDistance)
-                {
-                    closestDistance = distance.magnitude;
-                    closestVector = transform.position - obj.transform.position;
-                    
-                    if (closestDistance < 10 && timeSinceLastDirectionChange >= changeDirectionInterval)
-                    {
-                        swim(minInterval, -closestVector.normalized);
-                    }
-                    if (closestDistance < 0.75)
-                    {
-                        Destroy(obj.gameObject);
-                    }
-                }
-            }
-        }
-
-        else if (timeSinceLastDirectionChange >= changeDirectionInterval)
-        {
-            swim(randomInterval, randomDirection);
-        }
-
+        catchFood();
         limitSpeed();
+        if(stomachSize > 0 && (Time.time - moneyTimer) >= moneyFrequency)
+        {
+            Instantiate(money, transform.position, Quaternion.identity);
+            moneyTimer = Time.time;
+        }
+        else if(stomachSize < 0)
+        {
+            print(Time.time - moneyTimer);
+            moneyTimer = Time.time;
+        }
     }
 
     void flip(Vector2 direction)
@@ -96,6 +74,10 @@ public class Fish : MonoBehaviour
         flip(direction);
         rb.AddForce(direction * acceleration * 100);
         timeSinceLastDirectionChange = 0.0f;
+        if (stomachHunger > 0)
+        {
+            stomachHunger -= movementCost;
+        }
     }
 
     void limitSpeed()
@@ -103,6 +85,63 @@ public class Fish : MonoBehaviour
         if (rb.velocity.magnitude > speed)
         {
             rb.velocity = rb.velocity.normalized * speed;
+        }
+    }
+    void directionSwitch()
+    {
+        if (Random.Range(1, changeDirectionMax) == changeDirectionMin)
+        {
+            movementDirection = Random.insideUnitCircle;
+        }
+    }
+
+    void catchFood()
+    {
+        timeSinceLastDirectionChange += Time.deltaTime;
+        GameObject[] findFood = GameObject.FindGameObjectsWithTag(targetTag);
+        Vector2 closestVector = new Vector2(0, 0);
+        float randomInterval = Random.Range(minInterval, maxInterval);
+
+        if (findFood.Length != 0 && stomachHunger < (hungerThreshold * stomachSize))
+        {
+            foreach (GameObject obj in findFood)
+            {
+                Vector2 distance = transform.position - obj.transform.position;
+
+                if (obj == findFood.Last())
+                {
+                    closestDistance = Mathf.Infinity;
+                }
+
+                if (distance.magnitude < closestDistance)
+                {
+                    closestDistance = distance.magnitude;
+                    closestVector = transform.position - obj.transform.position;
+
+                    if (closestDistance < 10 && timeSinceLastDirectionChange >= changeDirectionInterval)
+                    {
+                        swim(minInterval, -closestVector.normalized);
+                    }
+                    if (closestDistance < 0.75)
+                    {
+                        print(stomachHunger + "/" + stomachSize);
+                        Destroy(obj.gameObject);
+                        if ((stomachHunger + foodBonus) > stomachSize)
+                        {
+                            stomachHunger = stomachSize;
+                        }
+                        else
+                        {
+                            stomachHunger += foodBonus;
+                        }
+                    }
+                }
+            }
+        }
+        else if (timeSinceLastDirectionChange >= changeDirectionInterval)
+        {
+            directionSwitch();
+            swim(randomInterval, movementDirection); ;
         }
     }
 }
